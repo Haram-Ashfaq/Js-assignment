b6b46ab674a41789f63a96f164d3c74ffa32050e
Haram-Ashfaq <145918695+Haram-Ashfaq@users.noreply.github.com>
2023-09-24 20:01:36 +0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,354 @@
+// chapter no. 1
+// question no .1
+// alert("First Name");
+// alert("Last Name");
+// alert("Email");
+// alert("Phone number");
+// alert("Password");
+// question no. 2
+// alert("you're learning JavaScript ");
+// question no.3
+// alert("Life goes on");
+
+// chapter no. 2
+// question no.1
+// var firstNum = 2;
+// question no.2
+// var message;
+// message = " You can do it";
+// question 3
+// var teammate = "All for the team";
+// alert(teammate);
+// question no.4
+// bestMan = "John";
+// Chapter no.3
+// question no.1
+// var caseQty;
+// question no.2
+// caseQty  = 144;
+// question no. 3
+// var num = 9;
+// question no.4
+// var numSum;
+// numSum = 2 + 2;
+// question no.5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+// question no.6
+// var ageFactor  = 16;
+// ageFactor = ageFactor  + 12;
+// Chapter no.4
+// question no.1
+// var productCost = 3.45;
+// question no. 2
+// var nameOfBand;
+// question no.3
+// var num1 = 4;
+// question no.4
+// var haramAshfaq;
+// question no.5
+// illegal
+// var 12num;
+// var num 12;
+// var num first;
+// legal
+// var num12;
+// var num_12;
+// var numFirst!
+// var num;
+// chapter no. 5
+// question no.1
+// % modulus operator
+// question no. 2
+// num = 2;
+// question no.3
+// var largeNum = 1000 * 2000;
+// question no 4
+// var num1 = 10;
+// var num2 = 5;
+// var x = num1 - num2;
+// question no. 5
+// var remainder = 19 % 2;
+// question no. 6
+// var num1 = 10 ;
+// var num2 = 15;
+// var num = num1 * num2;
+// alert(num);
+// chapter no.6
+// question no.1
+// var xy = x++;
+// question no.2
+// var xy = x--;
+// question no.3
+// 51
+// question no.4
+// 49
+// question no.5
+// var num = 1;
+// var newNum = num--;
+// question no.6
+// var num = 5;
+// var newNum = num++;
+// question no.7
+// var num = 57;
+// var newNum = num++;
+// alert(num); 
+// Chapter no.7
+// question no.1
+// 14
+// question no.2
+// 24
+// question no.3
+// 24
+// question no.4
+// 18
+// question no.5
+// var calculatedNum =((2 + 2) * (4 + 10));
+// question no.6
+// var calculatedNum = (2 + (2 * 4) + 10);
+// question no.7
+// Chapter no.8
+// question no.1
+// "22"
+// question no.2
+// alert("Hello,Dolly");
+// question no.3
+// 333
+// question no.4
+// alert("Pakistan " + "Zindabaad");
+// question no.5
+// var message = "your score is " + 19;
+// question no.6
+// var firstName = "Haram";
+// var last name = "Ashfaq";
+// var your ID = firstName + lastName;
+// Chapter 9
+// question no.1
+// var firstName = prompt("Enter your first name");
+// question no .2
+// var countryName = prompt("Country","china);
+// question no 3
+// var yourName = prompt("Enter your name");
+// question no 4
+// var mood = prompt("Are you happy","no");
+// question no5
+// var test = "What is your test score";
+// var score = 150;
+// var result = prompt(test,score);
+// question no.6
+// var favCountry = prompt("What is your favorite country","China");
+// alert(favCountry);
+// chapter no.10
+// question no.1
+// var city = "Karachi";
+// if (city === "Karachi") {
+//   console.log("The city of light");
+// }
+// question no.2
+// if (x === y) {
+//   var two = alert(prompt("What is the value of z"));
+// question no.3
+// if (Zip code === "10010") {
+//   alert("Karachi");
+// }
+// else {
+//   alert("Please write correct city");
+// }
+// question no.4
+// if (x === 15) {
+//   x = 17;
+// }
+// Chapter 11
+// question no.1
+// if (x !== y) {
+// question no.2
+// if (x >= y) {
+// question no.3
+// if (x !== 5) { 
+//   x = 10;
+// }
+// question no.4
+// if (10 !== 15) {
+//   alert("Congratulations");
+// }
+// question no.5
+// var name = prompt("Enter the name");
+// if (name !== "Mike") {
+//  alert("no match");
+// }
+// chapter no.12 
+// question no 1
+// if (x >= y) {
+//   alert("it is greater and equal");
+// }
+// else {
+//   alert("it is not greater and equal");
+// }
+// question no.2
+// var marks = prompt("Enter your marks");
+// var percent =((marks/1100) * 100));
+// if (percent >= 85) {
+//   alert("Your Grade is A");
+// }
+// else if (percent < 85 && percent >= 70) {
+//   alert("Your Grade is B");
+// }
+// else if (percent < 70 && percent >= 60) {
+//   alert("Your Grade is C");
+// }
+// else {
+//   alert("Your Grade is D");
+// }
+// question no 3
+// if (a !== 10) {
+//   alert("a value is " + 10);
+// }
+// question no.4
+// var city = prompt("Enter the city");
+// if (city === "Karachi") {
+//   alert("It is Karachi");
+// }
+// else if (city === "Lahore") {
+//   alert("it is Lahore");
+// }
+// else {
+//   alert("it is neither Karachi nor Lahore");
+// }
+// Chapter no 13
+// question no 1
+// if (a === b && c === d) {
+// question no 2
+// if (a  === b || c !== d) {
+// question no 3
+// if ((name === "Hamza" || name === "Arslan") && age < 60) {
+// question no. 4
+// var x = 50;
+// var y = 60;
+// if (x < y || x > y) {
+//    alert("OK");
+// }
+// question no 5
+// var firstName = "Haram";
+// var lastName = "Ashfaq";
+// var inputFirst = prompt("Enter your first name");
+// var inputLast = prompt("Enter your last");
+// if (inputFirst === firstName && inputFirst === lastName) {
+//   alert("Your name is Haram Ashfaq");
+// }
+// Chapter no 14
+// question no.1
+// if (password !== "") {
+//   if (password < 5) {
+//    alert("Password must be greater than 5");
+//   }
+//      if (password > 5) {
+//       alert("OK");
+//      }
+// }
+// question no 2
+// if (a === 1) {
+//   if (c === "Max") {
+//    alert("Ok");
+//   }
+// }
+// question no.3
+// if (a ===1 &&  c === "Max") {
+//   alert("OK");
+// }
+// question no 4
+// var num1 = 20;
+// var num2 = 20;
+// if (num1 === num2) {
+//   if(num1 <= num2) {
+//     alert("OK");
+//   }
+// }
+// Chapter no 15
+// question no 1
+// var arr = [ ];
+// question no 2
+// var arr = ["home"];
+// question no 3
+// alert(arr[2]);
+// question no 4
+// var alphabet = ["h","i","j","k","l","m","n","o"];
+// alert(alphabet.length);
+// question no 5
+// var arr = ['index','initialization']
+// arr.push("increment");
+// alert(arr[2]);
+// chapter no. 16
+// question no 1
+//var animals = ['lion']
+//animals.push('zebra');
+//alert(animals[1]);
+//question no 2
+//var alphabet = ['h','i','j','k',]
+//alphabet.pop();
+//alert(alphabet);
+//question no 3
+//var Alphabet = ['h','i','j','k']
+//Alphabet.push('l',1);
+//alert(Alphabet);
+// Chapter 16 (Array III)
+//question no.1
+// var sizes = ['S','M','XL','XXL','XXXL']
+// sizes.shift();
+// //question no.2
+// sizes.unshift('XL','XXL','XXXl');
+// //question no.3
+// var animals = ['lion']
+// animals.unshift('zebra');
+// alert(animal[0]);
+// //question no. 4
+// var sizes = ['S','M','XL','XXL','XXXL']
+// sizes.splice(2,1,"L");
+// alert(sizes);
+// question no.5
+// var sizes = ['S','M','XL','XXL','XXXL']
+// var regSizes = sizes.splice(0,3);
+// alert(regSizes);
+// question no.6
+// var pets = ['dogs','cats','ox','duck','frog']
+// pets.splice(1,3,"lizard",'chameleon');
+// alert(pets);
+//question no.7
+// var pets = ['dogs','cat','ox','duck','frog']
+// pets.splice(1,2);
+// alert(pets);
+// question no.8
+// var pets = ['dog','cat','ox','duck','frog','flea']
+// var big = pets.slice(3,5);
+// alert(big);
+// Chapter no. 17 to 20
+// question no. 1
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+// question no. 2
+// for (i = 0; i <= 11; i++) {
+// question no.3
+//(),;;,{}
+//for (var i = 0; i <=0; i++) {
+// question no.4
+//for (var x = 0; x < 100; x++ ) {
+     //console.log(x)
+//}
+//question no.5
+// for (i = 10; i > 7; i--) {
+//      console.log(i);
+// }
+//question no. 6
+//var pets = ['lion','zebra','blahblah','fufu']
+//var x = pets.length;
+//alert(x)
+// question no.7
+//var flag = true;
+//question no.8
+//var pets = ['dog','cat','ox','duck','frog','flea'];
+//for (var i = 1; i <= pets.length; i++) {
+     console.log(1);
+//}
+//question no. 9
